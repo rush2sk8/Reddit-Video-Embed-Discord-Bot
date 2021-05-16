25d75fcd2c89e9b4a389e43630c9d43612cd9d4f
rushadantia <rushadantia@gmail.com>
2021-05-16 18:02:07 -0400

    removed some ffmpeg

--- bot.py
+++ bot.py
@@ -1,28 +1,33 @@
 import os
 import re
+import subprocess
+import sys
+import time
 import urllib.request
 from pathlib import Path
+from shutil import which
 
 import discord
-import sys
 import requests
 from discord.ext import commands
-import subprocess
 from dotenv import load_dotenv
-from shutil import which
+from pystreamable import StreamableApi
 
 load_dotenv(dotenv_path=Path('.')/'.env')
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+S_USER = os.getenv('S_USER')
+S_PASS = os.getenv('S_PASS')
+
 
 bot = commands.Bot(command_prefix='!', intents= discord.Intents.default())
+streamable_api = StreamableApi(S_USER, S_PASS)
 
 @bot.event
 async def on_ready():
     """Event when the bot logs in"""
     await bot.change_presence(activity=discord.Streaming(name="by rush2sk8", url='https://www.twitch.tv/rush2sk8'))
     print('We have logged in as {0.user}'.format(bot))
-
 
 @bot.event
 async def on_message(message):
@@ -35,6 +40,8 @@ async def on_message(message):
             try:
                 json = requests.get(url, headers = {'User-agent': 'reddit-discordbot'}).json()
                 video_url = json[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
+
+                video_title = json[0]["data"]["children"][0]["data"]["title"]
 
                 l = video_url.split("DASH")[0]
                 r = video_url.split("?")[-1]
@@ -49,7 +56,7 @@ async def on_message(message):
                     urllib.request.urlretrieve(audio_url, filename="audio.mp4")
 
                     subprocess.call (
-                        "ffmpeg -i video.mp4 -i audio.mp4 -c:v copy -c:a aac output.mp4",
+                        "ffmpeg -y -hide_banner -loglevel error -i video.mp4 -i audio.mp4 -c:v copy -c:a aac output.mp4",
                         shell=True
                     )
 
@@ -59,11 +66,12 @@ async def on_message(message):
                 except Exception:
                     pass
 
-                subprocess.call (
-                    f"./ffmpeg_reduce_size.sh {final_video_name} 8"
-                )
+                shortcode = streamable_api.upload_video(final_video_name, video_title)['shortcode']
 
-                await message.channel.send(file=discord.File(f'./{final_video_name}'))
+                while streamable_api.get_info(shortcode)["percent"] != 100:
+                    time.sleep(1)
+
+                await message.channel.send(f'https://streamable.com/{shortcode}')
 
                 os.remove(final_video_name)
                 os.remove("video.mp4")
